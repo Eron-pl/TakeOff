cc808dac1006a543fab6c07eea345fbd52da7367
owarin <mateusz.nawiesniak@gmail.com>
2021-11-17 11:44:52 +0100

    Podstawowe pobieranie
    Możliwość użycia WebClient do pobrania pliku za pomocą funkcji Downloader.Download()

--- Downloader.cs
+++ Downloader.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
+using System.Net;
+using System.Diagnostics;
 
 namespace TakeOff
 {
@@ -11,7 +12,12 @@ namespace TakeOff
     {
         void Download(string location, string url)
         {
+            //location = local path to where the file has to be saved (including name and extension)
+            //url = web url to direct file download
+            WebClient client = new WebClient();
 
+            client.DownloadFile(url, location);
+            Debug.WriteLine($"Pobieram plik, po pobraniu będzie można go znaleźć w {location}");
         }
     }
 }
