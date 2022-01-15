8cffbcea93ac52ad024e5567016cc08498e60087
owarin <mateusz.nawiesniak@gmail.com>
2022-01-15 17:53:01 +0100

    Zaimplementowano zliczanie zaznaczonych paneli oraz dynamiczne wyświetlanie przycisku

--- Views/DownloadView.xaml.cs
+++ Views/DownloadView.xaml.cs
@@ -7,12 +7,12 @@ using TakeOff.Resources;
 using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel;
 
 namespace TakeOff.Views
 {
     public partial class DownloadView : UserControl
     {
-        
         public DownloadView()
         {
             InitializeComponent();
@@ -28,7 +28,6 @@ namespace TakeOff.Views
 
             // Utworzenie event'u przy zmianie wartości w search box'ie
             SearchBox.TextChanged += new TextChangedEventHandler( SearchBoxTextChanged );
-
         }
 
         // Obługa event'u przy zmianie wartości w search box'ie
@@ -58,7 +57,7 @@ namespace TakeOff.Views
             }
         }
 
-        //Obsługa przycisku pobierania
+        // Obsługa przycisku pobierania
         private void DwnldBtn_Click(object sender, RoutedEventArgs e)
         {
             foreach (Program program in icProgramsList.Items)
@@ -68,6 +67,28 @@ namespace TakeOff.Views
                 {
                     program.Download();
                 }
+            }
+        }
+
+        // Zliczanie zaznaczonych paneli
+        int checkedCount = 0;
+        private void TglBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            checkedCount += 1;
+            Console.WriteLine(checkedCount);
+            DwnldBtn.Visibility = Visibility.Visible;
+            DwnldBtn.Content = $"Pobierz ({checkedCount})";
+        }
+
+        private void TglBtn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            checkedCount -= 1;
+            Console.WriteLine(checkedCount);
+            DwnldBtn.Content = $"Pobierz ({checkedCount})";
+
+            if(checkedCount == 0)
+            {
+                DwnldBtn.Visibility = Visibility.Hidden;
             }
         }
     }
