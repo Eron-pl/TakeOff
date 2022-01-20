27a77d571e7b918004ad350ea04f5ac0bb119c40
Patryk Sablik <45714197+Eron-pl@users.noreply.github.com>
2022-01-20 20:04:23 +0100

    Naprawa wyszukiwania

--- Resources/Program.cs
+++ Resources/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TakeOff.Resources
 {
-    class Program
+    public class Program: INotifyPropertyChanged // ViewModel
     {
         public string Name { get; set; }
         public string Version { get; set; }
         public string DownloadLink { get; set; }
         public string ImagePath { get; set; }
-        public bool isSelected { get; set; }
 
+        private bool _isSelected;
+        public bool IsSelected 
+        { 
+            get => _isSelected;
+            set => SetValue(ref _isSelected, value, nameof(IsSelected)); 
+        }
 
-        // DO ZROBIENIA
-        //
-        //public bool IsInstalled(string Name)
-        //{
-        //    return;
-        //}
+        private string _visibility;
+        public string Visibility
+        {
+             get => _visibility;
+             set => SetValue(ref _visibility, value, nameof(Visibility));
+        }
 
         public void Download ()
         {
@@ -32,10 +38,24 @@ namespace TakeOff.Resources
             Downloader.Download(DownloadLink, path);
         }
 
-        //public void Install (string PathToFile)
-        //{
+        private void SetValue(ref string value, string newValue, string propertyName)
+        {
+            if (value == newValue)
+                return;
+            value = newValue;
+            if (PropertyChanged is not null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
-        //}
+        private void SetValue(ref bool value, bool newValue, string propertyName)
+        {
+            if (value == newValue)
+                return;
+            value = newValue;
+            if (PropertyChanged is not null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
