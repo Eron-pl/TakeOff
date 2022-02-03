fc8d0717a179d6af325932576d753f05b9418604
Patryk Sablik <45714197+Eron-pl@users.noreply.github.com>
2022-02-03 18:58:55 +0100

    Dodanie testu Installer'a

---
+++ Installer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeOff
+{
+    public class Installer
+    {
+        public static bool Install(string url)
+        {
+            try
+            {
+                var path = @"C:\TakeOff Downloads\" + url;
+                Process.Start(path);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
