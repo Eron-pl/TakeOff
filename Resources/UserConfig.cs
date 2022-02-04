8128f87ae1253b28e7724825c85474a3538e8b27
Patryk Sablik <45714197+Eron-pl@users.noreply.github.com>
2022-02-04 02:21:49 +0100

    Poprawa zmiany wybierania ścieżki zapisu programów

---
+++ Resources/UserConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeOff.Resources
+{
+    public class UserConfig
+    {
+        private string _programsInstalationPath;
+
+        public string ProgramsInstalationPath
+        {
+            get
+            {
+                return _programsInstalationPath;
+            }
+            set
+            {
+                _programsInstalationPath = value;
+            }
+        }
+    }
+}
--- Views/ConfigurationView.xaml.cs
+++ Views/ConfigurationView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +14,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using TakeOff.Resources;
 
 namespace TakeOff.Views
 {
     public partial class ConfigurationView : UserControl
     {
+        private UserConfig _userConfig;
         public ConfigurationView()
         {
             InitializeComponent();
+
+            string userConfigData = File.ReadAllText(@"Assets/UserConfig.json");
+            _userConfig = JsonConvert.DeserializeObject<UserConfig>(userConfigData);
+            ChoosenPathTextBlock.Text = _userConfig.ProgramsInstalationPath;
+            ApplyChangesBtn.Visibility = Visibility.Hidden;
         }
 
         private void PathChooseBtn_Click(object sender, RoutedEventArgs e)
@@ -30,8 +38,20 @@ namespace TakeOff.Views
             if (result.ToString() != string.Empty)
             {
                 ChoosenPathTextBlock.Text = openFileDlg.SelectedPath;
+                _userConfig.ProgramsInstalationPath = ChoosenPathTextBlock.Text;
+                ApplyChangesBtn.Visibility = Visibility.Visible;
             }
-          
+        }
+
+        private void ApplyChangesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string userConfigSerializedData = JsonConvert.SerializeObject(_userConfig);
+
+            StreamWriter myStream = new StreamWriter(@"Assets/UserConfig.json", append: true);
+            myStream.WriteLine(userConfigSerializedData);
+            myStream.Close();
+
+            //File.WriteAllText("Assets/UserConfig.json", userConfigNewData);
         }
     }
 }
